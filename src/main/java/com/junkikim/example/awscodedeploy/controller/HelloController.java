d19b54e75924cbb83ee3c14cc5a68805c6329bab
Junki Kim <zxc7023@gmail.com>
2020-06-12 23:39:32 +0900

    HelloController 추가

---
+++ src/main/java/com/junkikim/example/awscodedeploy/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.junkikim.example.awscodedeploy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+	@ResponseBody
+	@RequestMapping("/hello")
+	public String hello() {
+		String result;
+		result = "hello";
+		return  result;
+	}
+}
