cd13d301f944ba62d3caded862f90f48c20bc3a9
Junki Kim <zxc7023@gmail.com>
2020-06-13 22:03:06 +0900

    스크립트파일 수정

--- deploy.sh
+++ deploy.sh
@@ -23,3 +23,26 @@ JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 echo "> JAR Name: $JAR_NAME"
 
 nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+
+#!/usr/bin/env bash
+
+REPOSITORY=/home/ubuntu/application/ExampleAwsCodeDeploy
+cd $REPOSITORY
+
+APP_NAME=ExampleAwsCodeDeploy
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+
+CURRENT_PID=$(pgrep -f $APP_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 종료할것 없음."
+else
+  echo "> kill -9 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> $JAR_PATH 배포"
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
